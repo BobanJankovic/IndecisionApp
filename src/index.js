2fd158fbd25c6e08aeb9f62f9f0894f62a5e5886
Boban <boban0092@gmail.com>
2018-09-03 12:08:42 +0200

    Rendering second way of doing

--- src/index.js
+++ src/index.js
@@ -46,6 +46,118 @@ const makeDecision = () => {
   alert(app.options[option]);
 }
 let broj=0;
+let visibility=false;
+
+const toogleVisibility = () => {
+    visibility=!visibility;
+    console.log("bokimoki");
+  rend();
+  }
+
+ 
+
+
+function rend(){
+  const template = (
+  <div>
+    <h1>{app.title}</h1>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+     <p>Duzina areja je : {app.options.length}</p>
+     
+    <ol>
+     {func}
+      
+      {visibility && 
+      <div>
+      <p>Boki moki je pravi developer</p>
+      </div>
+      }
+     
+     
+      
+    </ol>
+    <button onClick={removeFunction}>Remove All</button>
+    <button id={"dugme"} onClick={toogleVisibility} >{visibility ? "Hide sth" : "View sth"}</button>
+    <button disabled = {app.options.length === 0} onClick={makeDecision}>What should i do?</button>
+    <form onSubmit={onFormSubmit}>
+    <input type="text" name="option" ></input>
+    <button >Add option</button>
+    </form>
+  </div>
+);
+
+const appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
+};
+
+
+
+
+
+
+
+rend();
+
+
+
+
+
+
+
+/*
+Second way of doing rendering pure JS
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+
+let func;
+const app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer',
+  options: []
+};
+
+const onFormSubmit = (e) => {
+  e.preventDefault();
+ 
+  let vrednost = e.target.elements.option.value; 
+  
+  if (vrednost) {
+    app.options.push(vrednost);
+    e.target.elements.option.value='';
+    
+  }
+  let index=0;
+  
+
+  func=app.options.map(element => {
+    //mozda najboje resenje za key
+ return <li key={index++}>{element}</li>
+});
+
+  rend();
+}
+
+
+
+const removeFunction = () => {
+  console.log("klik na remove");
+  app.options=[];
+  func=[];
+  rend();
+}
+
+const makeDecision = () => {
+  let option= Math.floor(Math.random()*app.options.length);
+  alert(app.options[option]);
+}
+let broj=0;
+
+
 const toogleVisibility = () => {
     broj++;
     if (broj%2!==0) {
@@ -89,7 +201,7 @@ function rend(){
       
     </ol>
     <button onClick={removeFunction}>Remove All</button>
-    <button id={"dugme"} onClick={toogleVisibility} >View sth</button>
+    <button id={"dugme"} onClick={toogleVisibility} >{broj%2!==0 ? "Hide sth" : "View sth"}</button>
     <button disabled = {app.options.length === 0} onClick={makeDecision}>What should i do?</button>
     <form onSubmit={onFormSubmit}>
     <input type="text" name="option" ></input>
@@ -117,25 +229,66 @@ rend();
 
 
 
-/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Exercise:
+
 const user = {
   name: 'Andrew',
   age: 26,
   location: 'Philadelphia'
 };
- //<li>{app.options[1]}</li>
-      <li>{app.options[2]}</li>
-      //nijie dobro
-      {app.options.forEach(element => {
-        <li>{element}</li>
-      })}
-
-<li>{app.options.forEach(element => {element})}</li>
-function getLocation(location) {
-  if (location) {
-    return <p>Location: {location}</p>;
-  }
-}
 
 const templateThree = (
   <div>
